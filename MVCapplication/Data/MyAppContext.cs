bb3872561e8ac9cefb85874fd59da281350cea65
carlospenaforte <carlosedu_1132@hotmail.com>
2025-03-17 20:54:44 -0300

    Connect App do DbContext and Create a SQL Server.

--- MVCapplication/Data/MyAppContext.cs
+++ MVCapplication/Data/MyAppContext.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MVCapplication.Models;
 
 namespace MVCapplication.Data
 {
     public class MyAppContext : DbContext
     {
+        public MyAppContext(DbContextOptions<MyAppContext> options) : base(options) { }
+
+        public DbSet<Item> Items { get; set; }
     }
 }
