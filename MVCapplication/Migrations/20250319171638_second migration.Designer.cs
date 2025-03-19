fbca4043d92206a4f5615e1aceb0ba1f3f85cd00
carlospenaforte <carlosedu_1132@hotmail.com>
2025-03-19 14:17:19 -0300

    crud overview. adding price data. add migration and update database

--- MVCapplication/Migrations/20250319171638_second migration.Designer.cs
+++ MVCapplication/Migrations/20250319171638_second migration.Designer.cs
@@ -3,6 +3,7 @@ using MVCapplication.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVCapplication.Migrations
 {
     [DbContext(typeof(MyAppContext))]
-    partial class MyAppContextModelSnapshot : ModelSnapshot
+    [Migration("20250319171638_second migration")]
+    partial class secondmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
